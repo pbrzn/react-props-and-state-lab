b7a6b948ff1b71d88dc7b2ab8227ca44424ed807
Patrick Brennan <pbrennanmusic@gmail.com>
2021-11-27 01:01:39 -0500

    commit tree 72b089a26e0e3bda15b29542ac3b555bb724da6c

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,45 @@ class App extends React.Component {
     }
   }
 
+  handleFilterChange = (event) => {
+    this.setState({
+      filters: {
+        type: event.target.value
+      }
+    })
+  }
+
+  handleFilterClick = () => {
+    if (this.state.filters.type === "all") {
+      fetch("/api/pets")
+      .then(resp => resp.json())
+      .then((json) => {
+        let formData = this.state.pets.concat(json)
+        this.setState({
+          pets: formData
+        })
+      })
+    } else {
+      fetch(`/api/pets?type=${this.state.filters.type}`)
+      .then(resp => resp.json())
+      .then(json => {
+        let formData = this.state.pets.concat(json)
+        this.setState({
+          pets: formData
+        })
+      });
+    }
+  }
+
+  handleAdoptPet = (id) => {
+    let adaptedPets = [...this.state.pets];
+    let pet = adaptedPets.find(pet => pet.id === id);
+    pet.isAdopted = true;
+    this.setState({
+      pets: adaptedPets,
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +63,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleFilterChange} onFindPetsClick={this.handleFilterClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.handleAdoptPet}/>
             </div>
           </div>
         </div>
